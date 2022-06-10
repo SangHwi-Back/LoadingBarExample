c3498212c1232e51ea89a75d22cfab178d2657f7
백상휘 <sanghwiback@gmail.com>
2022-06-10 09:10:06 +0900

    Initial Commit from AppCode

---
+++ LoadingBarExample/LoadingBarExample/Supports/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  LoadingBarExample
+//
+//  Created by 백상휘 on 2022/06/09.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ LoadingBarExample/LoadingBarExample/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  LoadingBarExample
+//
+//  Created by 백상휘 on 2022/06/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+}
